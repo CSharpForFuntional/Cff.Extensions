69206002056dae62cb021d71056a798124f514d1
SeungYongShim <syshim@me.com>
2023-04-22 22:01:58 +0900

    .

--- src/Cff.Extensions.AspNetCore/Effects/IHasValidator.cs
+++ src/Cff.Extensions.AspNetCore/Effects/IHasValidator.cs
@@ -6,14 +6,8 @@ public interface IHasValidator<RT> : IHas<RT, IValidator> where RT : struct, IHa
 {
     public static Aff<RT, Unit> ValidateAff<T>(T req) =>
         from validator in Eff
-        from _ in Aff(async () =>
-        {
-            var ret = await validator.ValidateAsync(new ValidationContext<T>(req));
-            return ret.IsValid switch
-            {
-                false => throw new ValidationException(ret.Errors),
-                _ => unit
-            };
-        })
+        from _ in Aff(() => validator.ValidateAsync(
+            ValidationContext<T>.CreateWithOptions(req, o => o.ThrowOnFailures())).ToUnit().ToValue()
+        )
         select unit;
 }
